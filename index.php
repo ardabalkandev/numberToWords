346135d7a22948f45e778d57d8e68e0253b5e847
Arda Balkan <ardabalkan@gmail.com>
2024-10-03 14:20:48 +0300

    fixes for teens in Turkish

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require('numberToWords.php');
 $converter = new NumberToWords();
-echo $converter->convert('987154321123,02', 'TR');
+echo $converter->convert('987114321123,02', 'TR');
 echo "<br>";
 echo $converter->convert('987154321123,9923', 'EN');
 ?>
--- numberToWords.php
+++ numberToWords.php
@@ -17,7 +17,7 @@ class NumberToWords {
         switch ($this->language) {
             case 'TR': // TÜRKÇE
                 $this->ones = [0 => 'SIFIR', 1 => 'BİR', 2 => 'İKİ', 3 => 'ÜÇ', 4 => 'DÖRT', 5 => 'BEŞ', 6 => 'ALTI', 7 => 'YEDİ', 8 => 'SEKİZ', 9 => 'DOKUZ'];
-                $this->teens = []; // Türkçede özel teen sayıları yok, tens ile normalde işlenecek.
+                $this->teens = [11 => 'ON BİR', 12 => 'ON İKİ', 13 => 'ON ÜÇ', 14 => 'ON DÖRT', 15 => 'ON BEŞ', 16 => 'ON ALTI', 17 => 'ON YEDİ', 18 => 'ON SEKİZ', 19 => 'ON DOKUZ'];
                 $this->tens = [10 => 'ON', 20 => 'YİRMİ', 30 => 'OTUZ', 40 => 'KIRK', 50 => 'ELLİ', 60 => 'ALTMIŞ', 70 => 'YETMİŞ', 80 => 'SEKSEN', 90 => 'DOKSAN'];
                 $this->scales = [1000000000 => 'MİLYAR', 1000000 => 'MİLYON', 1000 => 'BİN', 100 => 'YÜZ'];
                 $this->decimalSeparator = 'VİRGÜL';
