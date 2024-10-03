253e6ab2bc77ef99d282a7d268b249f10f3350b8
Özkan Özcan <grafikerozkan@gmail.com>
2024-10-03 19:19:12 +0300

    - Dil spesifik sayısal terimler harici dosyalara taşındı (`languages/tr.php`, `languages/en.php`), bu sayede bakım ve genişletilebilirlik iyileştirildi. - Dil dosyası bulunamadığında varsayılan dilin (İngilizce) yüklenmesi için bir geri dönüş mekanizması eklendi. - Sert kodlanmış ifadeler (örneğin 'YÜZ') dil dosyalarından dinamik olarak çekilerek tüm dil spesifik metinlerin harici kaynaklardan yönetilmesi sağlandı. - Türkçe için 'BİR BİN' yerine doğru şekilde 'BİN' ifadesinin kullanılmasını sağlayacak kontroller eklendi. - Ondalık ayıracı doğru şekilde her dil için dinamik olarak belirlendi ('VİRGÜL' Türkçe, 'POINT' İngilizce için). - Yüzler, onluklar ve büyük sayılar için tekrar eden kodlar temizlenerek daha modüler hale getirildi, böylece kod okunabilirliği ve tekrar kullanılabilirlik arttı.
    Bu refaktör ile sınıfın esnekliği arttırıldı, yeni dillerin eklenmesi ve mevcut dillerin düzenlenmesi kolaylaştırıldı.

---
+++ languages/EN.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'ones' => [0 => 'ZERO', 1 => 'ONE', 2 => 'TWO', 3 => 'THREE', 4 => 'FOUR', 5 => 'FIVE', 6 => 'SIX', 7 => 'SEVEN', 8 => 'EIGHT', 9 => 'NINE'],
+    'teens' => [11 => 'ELEVEN', 12 => 'TWELVE', 13 => 'THIRTEEN', 14 => 'FOURTEEN', 15 => 'FIFTEEN', 16 => 'SIXTEEN', 17 => 'SEVENTEEN', 18 => 'EIGHTEEN', 19 => 'NINETEEN'],
+    'tens' => [10 => 'TEN', 20 => 'TWENTY', 30 => 'THIRTY', 40 => 'FORTY', 50 => 'FIFTY', 60 => 'SIXTY', 70 => 'SEVENTY', 80 => 'EIGHTY', 90 => 'NINETY'],
+    'scales' => [1000000000 => 'BILLION', 1000000 => 'MILLION', 1000 => 'THOUSAND', 100 => 'HUNDRED'],
+    'decimal_separator' => 'POINT'
+];
+?>
---
+++ languages/TR.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'ones' => [0 => 'SIFIR', 1 => 'BİR', 2 => 'İKİ', 3 => 'ÜÇ', 4 => 'DÖRT', 5 => 'BEŞ', 6 => 'ALTI', 7 => 'YEDİ', 8 => 'SEKİZ', 9 => 'DOKUZ'],
+    'teens' => [11 => 'ON BİR', 12 => 'ON İKİ', 13 => 'ON ÜÇ', 14 => 'ON DÖRT', 15 => 'ON BEŞ', 16 => 'ON ALTI', 17 => 'ON YEDİ', 18 => 'ON SEKİZ', 19 => 'ON DOKUZ'],
+    'tens' => [10 => 'ON', 20 => 'YİRMİ', 30 => 'OTUZ', 40 => 'KIRK', 50 => 'ELLİ', 60 => 'ALTMIŞ', 70 => 'YETMİŞ', 80 => 'SEKSEN', 90 => 'DOKSAN'],
+    'scales' => [1000000000 => 'MİLYAR', 1000000 => 'MİLYON', 1000 => 'BİN', 100 => 'YÜZ'],
+    'decimal_separator' => 'VİRGÜL'
+];
+?>
