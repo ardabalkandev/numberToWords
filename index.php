49cc339236b0174c665762880d9fbb7a9fa753a8
Arda Balkan <ardabalkan@gmail.com>
2024-10-03 14:10:16 +0300

    test values changed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require('numberToWords.php');
 $converter = new NumberToWords();
-echo $converter->convert('12237842,53', 'TR');
+echo $converter->convert('987154321123,02', 'TR');
 echo "<br>";
-echo $converter->convert('14111842,53', 'EN');
+echo $converter->convert('987154321123,02', 'EN');
 ?>
